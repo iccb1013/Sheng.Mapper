2d37309c0b57831ca8028434549e6ec52d05978e
liuhaoyang <liuhaoyang1221@hotmail.com>
2017-06-16 13:51:37 +0800

    Add Benchmark for Sheng.Mapper

---
+++ Sheng.Mapper.Benchmark/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleBenchmark.Core;
+
+namespace Sheng.Mapper.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Initialize();
+
+            BenchmarkRunner.Run<MapperBenchmarks>();
+
+            Console.ReadKey();
+        }
+    }
+}
